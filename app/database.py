eb314c2f03380d7559863158bfd40b944354a427
djenkins452 <dannyjenkins71@gmail.com>
2025-11-29 08:48:05 -0500

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,17 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-# Render will inject DATABASE_URL env variable
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base = declarative_base()
+
+
+# Dependency to get DB session
 def get_db():
     db = SessionLocal()
     try:
