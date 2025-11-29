b1a8f1c63460431d2905a6a498ed4b0a7fd32564
djenkins452 <dannyjenkins71@gmail.com>
2025-11-29 08:04:26 -0500

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,1 +1,10 @@
+from fastapi import FastAPI
+from app.routers import health
 
+app = FastAPI(title="Life Engine API")
+
+app.include_router(health.router)
+
+@app.get("/")
+def root():
+    return {"message": "Life Engine API is running!"}
