ad6cba2003475471b2bc28437b1f00b586c7f206
djenkins452 <dannyjenkins71@gmail.com>
2025-11-29 08:40:12 -0500

    Update jwt_handler.py

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,1 +1,14 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "LIFEENGINE_SUPER_SECRET_KEY"   # Change later!
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 60 * 24  # 24 hours
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    encoded = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded
 
