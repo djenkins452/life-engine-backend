a459f574d07600b068498c0093070c55e1c83288
djenkins452 <dannyjenkins71@gmail.com>
2025-11-29 08:39:21 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -3,3 +3,13 @@ from sqlalchemy.orm import declarative_base
 Base = declarative_base()
 
 # Add models later (User, Glucose, Fasting, Weight, etc.)
+
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
