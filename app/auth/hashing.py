9a8ae2c209e50ed0c7ff2dc0ea2840f1c204ab04
djenkins452 <dannyjenkins71@gmail.com>
2025-11-29 08:39:49 -0500

    Update hashing.py

--- app/auth/hashing.py
+++ app/auth/hashing.py
@@ -1,1 +1,13 @@
+from passlib.context import CryptContext
+
+pwd_cxt = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class Hasher:
+    @staticmethod
+    def hash_password(password: str):
+        return pwd_cxt.hash(password)
+
+    @staticmethod
+    def verify_password(plain_password: str, hashed_password: str):
+        return pwd_cxt.verify(plain_password, hashed_password)
 
