80ec75bf47987c87815c9183e9532ee1d4aad8eb
djenkins452 <dannyjenkins71@gmail.com>
2025-11-29 09:45:21 -0500

    Update schemas.py

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
 class RegisterRequest(BaseModel):
-    email: EmailStr
+    email: str
     password: str
 
 class LoginRequest(BaseModel):
-    email: EmailStr
+    email: str
     password: str
 
 class TokenResponse(BaseModel):
